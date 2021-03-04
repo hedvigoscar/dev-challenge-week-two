354190937bb062c9875d6067d7e52609a184e1e8
Oscar Nylander <oscar@hedvig.com>
2021-03-04 08:29:24 +0100

    Implement the countdown!

--- README.md
+++ README.md
@@ -1,27 +1,22 @@
-# Template repository
+# Countdown
 
-Template repository for the Jetpack Compose [#AndroidDevChallenge](https://developer.android.com/dev-challenge).
+![Workflow result](https://github.com/hedvigoscar/dev-challenge-week-two/workflows/Check/badge.svg)
 
-## Getting started
-Copy this repository by pressing the "Use this template" button in Github.
-Clone your repository and open it in the latest [Android Studio (Canary build)](https://developer.android.com/studio/preview).
 
-## Submission requirements
-- Follow the challenge description on the project website: [developer.android.com/dev-challenge](https://developer.android.com/dev-challenge)
-- All UI should be written using Jetpack Compose
-- The Github Actions workflow should complete successfully
-- Include two screenshots of your submission in the [results](results) folder. The names should be
-  screenshot_1.png and screenshot_2.png.
-- Include a screen record of your submission in the [results](results) folder. The name should be
-  video.mp4
-- Replace the contents of [README.md](README.md) with the contents of [README-template.md](README-template.md) and fill out the template.
+## :scroll: Description
+A simple countdown app. It has a dual-purpose button to start and stop the counter.
 
-## Code formatting
-The CI uses [Spotless](https://github.com/diffplug/spotless) to check if your code is formatted correctly and contains the right licenses.
-Internally, Spotless uses [ktlint](https://github.com/pinterest/ktlint) to check the formatting of your code.
-To set up ktlint correctly with Android Studio, follow one of the [listed setup options](https://github.com/pinterest/ktlint#-with-intellij-idea).
 
-Before committing your code, run `./gradlew app:spotlessApply` to automatically format your code.
+## :bulb: Motivation and Context
+I found it especially interesting to work with coroutines to create the ticker powering the
+countdown. I've used out-of-the-box ticker solutions from Rx before, and I'm always surprised
+at how easy some of these things are to implement yourself using coroutines.
+
+It's also nice to see how easy you can get some state that is saved using `rememberSaveable`,
+Compose continues to be a joy to build stuff in, quickly and correctly.
+
+## :camera_flash: Screenshots
+<img src="/results/screenshot_1.png" width="260">&emsp;<img src="/results/screenshot_2.png" width="260">
 
 ## License
 ```
--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,12 +18,33 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import java.util.Timer
+import java.util.TimerTask
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,8 +60,82 @@ class MainActivity : AppCompatActivity() {
 // Start building your app here!
 @Composable
 fun MyApp() {
+    val counterState = rememberSaveable { mutableStateOf<Int?>(null) }
+    val (counter, setCounter) = counterState
+    val scope = rememberCoroutineScope()
+    val tickState = remember { mutableStateOf<Job?>(null) }
+    val (tickJob, setTickJob) = tickState
+
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            counter?.let { c ->
+                Crossfade(targetState = c) {
+                    Text(
+                        it.toString(),
+                        style = MaterialTheme.typography.h5,
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    )
+                }
+            } ?: Text(
+                "Not started",
+                style = MaterialTheme.typography.h5,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+            Button(
+                onClick = {
+                    if (tickJob != null) {
+                        tickJob.cancel()
+                        setTickJob(null)
+                        return@Button
+                    }
+                    if (counter == null || counter == 0) {
+                        setCounter(10)
+                    }
+                    setTickJob(
+                        ticker(1000)
+                            .onEach {
+                                val current = counterState.component1()
+                                current?.let { c ->
+                                    setCounter(c - 1)
+                                    if (c - 1 == 0) {
+                                        val tj = tickState.component1()
+                                        tj?.cancel()
+                                        setTickJob(null)
+                                    }
+                                }
+                            }
+                            .launchIn(scope)
+                    )
+                }
+            ) {
+                Text(tickJob?.let { "Stop countdown" } ?: "Start countdown")
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalCoroutinesApi::class)
+fun ticker(intervalMs: Long) = callbackFlow {
+    val timer = Timer()
+    timer.scheduleAtFixedRate(
+        timerTask {
+            runCatching {
+                offer(Unit)
+            }
+        },
+        intervalMs, intervalMs
+    )
+
+    awaitClose { timer.cancel() }
+}
+
+inline fun timerTask(crossinline task: () -> Unit) = object : TimerTask() {
+    override fun run() {
+        task()
     }
 }
 
